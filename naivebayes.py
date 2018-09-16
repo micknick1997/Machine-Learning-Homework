42232dbb73ce087bae6beadbee5c831d67e505a5
micknick1997 <micknick1997@gmail.com>
2018-09-16 19:53:16 -0400

    Progress up to 09/16 afternoon

--- naivebayes.py
+++ naivebayes.py
@@ -1,1 +1,21 @@
 import numpy as np
+
+def main():
+    vocabList = []
+    docNum = 0
+    vocabFile = open("data/vocabulary.txt", "r")
+
+    vocab = vocabFile.readlines()
+    for word in vocab:
+        vocabList.append(word)
+    #print(len(vocabList))
+
+    trainingDocsFile = open("data/traininglabels.txt", "r")
+    trainingDocs = trainingDocsFile.readlines()
+    for line in trainingDocs:
+        docNum += 1
+    print(docNum)
+
+
+
+main()
