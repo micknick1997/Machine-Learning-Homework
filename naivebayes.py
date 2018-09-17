38d2c2012679149fea4115f214cd02c608a2f52d
Nick Martin <micknick1997@gmail.com>
2018-09-17 00:18:10 -0400

    Progress for 09/16 evening

--- naivebayes.py
+++ naivebayes.py
@@ -1,21 +1,45 @@
 import numpy as np
+from collections import Counter
 
-def main():
-    vocabList = []
-    docNum = 0
-    vocabFile = open("data/vocabulary.txt", "r")
+def probOfY(y):
+    totalCount = 0
+    specificCount = 0
+    
+    labelFile = open("data/traininglabels.txt", "r")
+    allLines = labelFile.readlines()
+    totalCount = len(allLines)
+    for line in allLines:
+        for char in line.split():
+            if char == str(y):
+                specificCount += 1
+    probability = specificCount/totalCount
+    labelFile.close()
+    return probability
 
-    vocab = vocabFile.readlines()
-    for word in vocab:
-        vocabList.append(word)
-    #print(len(vocabList))
+def probOfXGivenY(y):
+    columnOne = 0
+    columnTwo = 1
+    columnThree = 2
+    columnData = []
+    trainingData = []
+    delimeter = " "
+    lookup=str(y)
+    trainingLabels = open("data/traininglabels.txt", "r")
+    for num, line in enumerate(trainingLabels, 1):
+        if lookup in line:
+            trainingData.append(lookup.split(delimeter)[0])
+    # with open("data/traininglabels.txt") as trainingLabels:
+    #     trainingLabels.read().split("\n")[lineNumbers]
+    # index = [x for x in range(len(labels)) if str(y) in labels[x].lower()]
+    trainingData = open("data/trainingdata.txt", "r")
+    lines = trainingData.readlines()
+    # Search column 1
+    for x in lines:
+        columnData.append(x.split(delimeter)[columnOne])
+    
+    trainingData.close()
+    #trainingLabels.close()
 
-    trainingDocsFile = open("data/traininglabels.txt", "r")
-    trainingDocs = trainingDocsFile.readlines()
-    for line in trainingDocs:
-        docNum += 1
-    #print(docNum)
+    print(trainingData)
 
-
-
-main()
+probOfXGivenY(2)
