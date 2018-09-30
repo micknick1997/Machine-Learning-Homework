7eb3b6841e8afe668f2e7969ab3226306d12e185
Nick Martin <micknick1997@gmail.com>
2018-09-30 17:23:12 -0400

    Progress from 09/30 afternoon

--- hw2/decisiontree.py
+++ hw2/decisiontree.py
@@ -10,16 +10,26 @@ from sklearn.metrics import confusion_matrix
 import graphviz
 
 # Relevant directories and filenames
-root = '~/Documents/csc3520/assignments/hw2' #edit as needed
+root = '~/Documents/Machine Learning Homework/hw2/' #edit as needed
 #
 # feel free to define other useful filename here
 #
 
-
 # Load data from relevant files
 
+attributes = np.loadtxt(os.path.expanduser(root) + "data/attributes.txt", str)
+trainingData = np.loadtxt(os.path.expanduser(root) + "data/trainingdata.txt", delimiter=',')
+trainingLables = np.loadtxt(os.path.expanduser(root) + "data/traininglabels.txt")
+testingData = np.loadtxt(os.path.expanduser(root) + "data/testingdata.txt", delimiter=',')
+testingLabels = np.loadtxt(os.path.expanduser(root) + "data/testinglabels.txt")
+
+
+print(trainingData.shape)
+print(len(trainingLables))
+print(len(attributes))
 
 # Train a decision tree via information gain on the training data
+
 
 
 # Test the decision tree
