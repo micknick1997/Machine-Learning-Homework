e6fa6b1e24c3ea70175d5cff8faf08342533c71b
micknick1997 <micknick1997@gmail.com>
2018-10-04 14:05:25 -0400

    Progress from 10/04 afternoon

--- hw2/decisiontree.py
+++ hw2/decisiontree.py
@@ -5,6 +5,7 @@ import os
 import sys
 import numpy as np
 from sklearn import tree
+#from sklearn.cross_validation import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix
 import graphviz
@@ -34,14 +35,15 @@ testingLabels = np.loadtxt(os.path.expanduser(root) + "data/testinglabels.txt")
 
 # Train a decision tree via information gain on the training data
 
-# print(sp.stats.entropy(trainingData))
-# print(sp.stats.entropy(trainingData).shape)
+# entropyOfData = sp.stats.entropy(trainingData, base=2)
+decisionTree = DecisionTreeClassifier(criterion="entropy")
+decisionTree.fit(trainingData, trainingLables)
 
 
 # Test the decision tree
-
+decisionTree.predict(testingData)
 
 # Compute the confusion matrix on test data
-
+print(confusion_matrix(trainingLables, testingLabels))
 
 # Visualize the tree using graphviz
